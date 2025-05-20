0ef2ae833a8b8956cf2be856ab374aa0944cc846
eduardo <zubieta.eduardo@uap.edu.bo>
2025-05-20 10:34:11 -0400

    agregar modelos para cursos y notas

--- cursos/models.py
+++ cursos/models.py
@@ -1,10 +1,22 @@
 from django.db import models
+from usuarios.models import Usuario, Estudiante
 
 # Create your models here.
 class Curso(models.Model):
-    pass
-
+  nombre=models.CharField(max_length=100)
+  descripcion=models.TextField(blank=True)
+  fecha_inicio=models.DateField()
+  fecha_fin=models.DateField()
+  #relacion uno a muchos, un profesor puede dictar muchos cursos y un curso es dictado por un profesor
+  profesor=models.ForeignKey(Usuario, on_delete=models.CASCADE,limit_choices_to={'rol':'profesor'},related_name='cursos_dictados')
+  def __str__(self):
+    return f"{self.nombre} -{self.fecha_inicio}"
 class Inscripcion(models.Model):
-    pass
+    #un estduiante puede inscribirse a muchos cursos y un curso puede tener muchos estudiantes
+    estudiante=models.ForeignKey(Usuario, on_delete=models.CASCADE, limit_choices_to={'rol':'estudiante'},related_name='inscripciones')
+    curso=models.ForeignKey(Curso, on_delete=models.CASCADE)
+    fecha_inscripcion=models.DateTimeField(auto_now_add=True)
     
+    def __str__(self):
+       return f"{self.estudiante.ci} -inscrito en el curso: {self.curso.nombre}"
     
--- notas/models.py
+++ notas/models.py
@@ -1,5 +1,19 @@
 from django.db import models
+from cursos.models import Inscripcion
 
 # Create your models here.
 class Nota(models.Model):
-    pass
+    inscripcion=models.OneToOneField(Inscripcion, on_delete=models.CASCADE)
+    nota1=models.DecimalField(max_digits=5, decimal_places=2)
+    nota2=models.DecimalField(max_digits=5, decimal_places=2)
+    nota_final=models.DecimalField(max_digits=5, decimal_places=2, blank=True, null=True)
+    aprobado=models.BooleanField(default=False)
+    def calcular_nota_final(self):
+        if self.nota1 and self.nota2:
+            self.nota_final = (self.nota1 + self.nota2) / 2
+            self.aprobado = self.nota_final >= 60
+            self.save() #porque usar save() para guardar los cambios en la base de datos
+        else:
+            raise ValueError("Las notas no pueden ser nulas")
+    def __str__(self):
+        return f"Notas de {self.inscripcion.estudiante.username} - Curso: {self.inscripcion.curso.nombre}"
